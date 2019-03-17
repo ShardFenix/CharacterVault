159943a052d1b0119fc668c5cae7576f1aa6d33c
ShardFenix <psyclone87@juno.com>
2019-03-17 12:33:42 -0500

    Copied the char sheet so I can improve the other one

--- packages.js
+++ packages.js
@@ -194,13 +194,51 @@ function listAllUnknownSpells(char,derived){
 	}
 	return result;
 }
-	
+
+function getAttributesBelow20(char){
+	var result=[];
+	if (char.attributes.str<20){
+		result.push("+1 Strength");
+	}
+	if (char.attributes.dex<20){
+		result.push("+1 Dexterity");
+	}
+	if (char.attributes.con<20){
+		result.push("+1 Constitution");
+	}
+	if (char.attributes.int<20){
+		result.push("+1 Intelligence");
+	}
+	if (char.attributes.wis<20){
+		result.push("+1 Wisdom");
+	}
+	if (char.attributes.cha<20){
+		result.push("+1 Charisma");
+	}
+	return result;
+}
+
 function addSpell(char,spellname,forClass){
 	
 }
 
+function getUnknownFeats(char){
+	var result=[];
+	for (var feat of window.feats){
+		var found=false;
+		for (var cfeat of char.feats){
+			if (cfeat.name===feat.name){
+				found=true;break;
+			}
+		}
+		if (!found){
+			result.push(feat.name);
+		}
+	}
+	return result;
+}
 
-window.languages=['Common','Elven','Dwarven','Gnomish','Halfling','Giant','Orc','Infernal','Primordial','Abyssal','Celestial','Draconic','Deep Speech','Sylvan','Undercommon'];
+window.languages=['Language: Common','Language: Elven','Language: Dwarven','Language: Gnomish','Language: Halfling','Language: Giant','Language: Orc','Language: Infernal','Language: Primordial','Language: Abyssal','Language: Celestial','Language: Draconic','Language: Deep Speech','Language: Sylvan','Language: Undercommon'];
 window.feats=[];
 window.spells=[];
 window.races=[];
@@ -224,6 +262,39 @@ window.skills=[
 	{name:"Stealth",attribute:"dex"},
 	{name:"Survival",attribute:"wis"}
 ];
+
+window.abilities=[
+	{
+		name:"Bardic Inspiration (d6)",
+		description:"As a bonus action, choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d6.\nOnce within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.",
+		charges:1,
+		onLongRest:function(char,derived,scope){
+			this.charges=derived.modifiers.cha;
+		}
+	},{
+		name:"Bardic Inspiration (d8)",
+		description:"As a bonus action, choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d8.\nOnce within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.",
+		charges:1,
+		onShortRest:function(char,derived,scope){
+			this.charges=derived.modifiers.cha;
+		}
+	},{
+		name:"Bardic Inspiration (d10)",
+		description:"As a bonus action, choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d10.\nOnce within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.",
+		charges:1,
+		onShortRest:function(char,derived,scope){
+			this.charges=derived.modifiers.cha;
+		}
+	},{
+		name:"Bardic Inspiration (d12)",
+		description:"As a bonus action, choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d12.\nOnce within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.",
+		charges:1,
+		onShortRest:function(char,derived,scope){
+			this.charges=derived.modifiers.cha;
+		}
+	}
+];
+
 window.passives=[
 	{
 		name:"Jack of All Trades",
@@ -400,6 +471,72 @@ window.classes=
 						"choices":[listSpecializations],
 						"action":function(char,derived,choice){
 							addSubclass(char,"Bard",choice);
+						}
+					}
+				]
+			}, { // 4
+				"updates":[
+					{
+						"choicePrompt":"What did you roll for your hit dice?",
+						"choices":[1,2,3,4,5,6,7,8],
+						"action":function(char,derived,choice){
+							char.hpMax+=choice;
+						}
+					}, {
+						"choicePrompt":"Choose one:",
+						"choices":["+2 to Ability Scores","Take a Feat"],
+						"action":function(char,derived,choice,$scope){
+							if (choice==="Take a Feat"){
+								$scope.updateStep+=2;
+							}
+						}
+					}, {
+						"choicePrompt":"Choose one:",
+						"choices":[getAttributesBelow20],
+						"action":function(char,derived,choice,$scope){
+							switch (choice){
+								case "+1 Strength":char.attributes.str+=1;break;
+								case "+1 Dexterity":char.attributes.dex+=1;break;
+								case "+1 Constitution":char.attributes.con+=1;break;
+								case "+1 Intelligence":char.attributes.int+=1;break;
+								case "+1 Wisdom":char.attributes.wis+=1;break;
+								case "+1 Charisma":char.attributes.cha+=1;break;
+							}
+						}
+					}, {
+						"choicePrompt":"Choose one:",
+						"choices":[getAttributesBelow20],
+						"action":function(char,derived,choice,$scope){
+							switch (choice){
+								case "+1 Strength":char.attributes.str+=1;break;
+								case "+1 Dexterity":char.attributes.dex+=1;break;
+								case "+1 Constitution":char.attributes.con+=1;break;
+								case "+1 Intelligence":char.attributes.int+=1;break;
+								case "+1 Wisdom":char.attributes.wis+=1;break;
+								case "+1 Charisma":char.attributes.cha+=1;break;
+							}
+							$scope.updateStep++;
+						}
+					}, {
+						"choicePrompt":"Choose a Feat",
+						"choices":[getUnknownFeats],
+						"action":function(char,derived,choice){
+							//todo
+						}
+					}
+				]
+			},{//5
+				"updates":[
+					{
+						"choices":[],
+						"action":function(char,derived){
+							//upgrade bardic inspiration to recharge on short rest
+							for (var abil of char.abilities){
+								if (abil.name==="Bardic Inspiration"){
+									
+									return;
+								}
+							}
 						}
 					}
 				]
