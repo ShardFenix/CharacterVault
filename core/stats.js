15833020bc599bd2b624e46555b63582a74cec77
travis schnell <travis.schnell@kemper.com>
2019-10-09 17:35:27 -0500

    sdfdsf

--- core/stats.js
+++ core/stats.js
@@ -253,7 +253,10 @@ function finishLevelUp(){
 	$scope.historyLevel=$scope.char.level;
 	prepForSave($scope.charBackup);
 	$scope.history[$scope.char.level-1]=$scope.charBackup;
-	$scope.history[$scope.char.level]=tempChar;
+	//only add new level to history if one isn't there already
+	if ($scope.history[$scope.char.level].level){
+		$scope.history[$scope.char.level]=tempChar;
+	}
 }
 
 
@@ -1225,13 +1228,16 @@ $scope.load=function(characterName){
 }
 
 $scope.loadFromLevelHistory=function(level){
+	let currentLevel = $scope.char.level;
 	if ($scope.history.length>level){
 		if ($scope.history[level].level===level){
 			//TODO: push current level into history if it's not there yet
 			$scope.char=angular.copy($scope.history[level]);
 			initLoadedCharacter($scope.char);
+			return;
 		}
 	}
+	$scope.historyLevel=currentLevel;
 }
 
 function initLoadedCharacter(char){
